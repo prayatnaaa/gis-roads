eb87c6362f3709ef828fc51c156d41d2919e2a54
Tude Prayatna <tudeprayatna04@gmail.com>
2025-05-20 01:58:49 +0800

    feat(add): setup leaflet and maps

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 function App() {
   return (
     <>
-      <div className="w-full h-screen flex justify-center items-center">
-        hello world
+      <div className="relative z-0 h-screen w-full">
+        <MapContainer
+          center={[-2.5489, 118.0149]}
+          zoom={6}
+          className="h-screen w-full"
+          style={{ zIndex: 0 }}
+          zoomControl={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
       </div>
     </>
   );
