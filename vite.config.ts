ddb21fefee5051eb854f1bab1c302f89703de0d5
Tude Prayatna <tudeprayatna04@gmail.com>
2025-05-20 02:04:46 +0800

    feat: setup shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { config } from "dotenv";
+import path from "path";
 
 config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // define: {
   //   "process.env": process.env,
   // },
