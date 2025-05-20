83ac4b2e6087954667248dfa102472311fda991c
Tude Prayatna <tudeprayatna04@gmail.com>
2025-05-20 23:47:54 +0800

    feat(token): manage token session

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
 import Home from "./components/pages/home-page";
 import AddRoad from "./components/pages/add-road-page";
 import Register from "./components/pages/register-page";
 import Login from "./components/pages/login-page";
+import React from "react";
 
 function App() {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!token) {
+      navigate("auth/login");
+    }
+  }, [token, navigate]);
+
   return (
     <>
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/add-road" element={<AddRoad />} />
-            <Route path="auth/register" element={<Register />} />
-            <Route path="auth/login" element={<Login />} />
-          </Routes>
-        </div>
-      </Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-road" element={<AddRoad />} />
+          <Route path="auth/register" element={<Register />} />
+          <Route path="auth/login" element={<Login />} />
+        </Routes>
+      </div>
     </>
   );
 }
